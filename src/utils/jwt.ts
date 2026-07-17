0a020acc646db279f4e536e8aba5156d18eb856f
Luvtse <89990285+Luvtse@users.noreply.github.com>
2026-07-17 17:24:29 +0300

    Delete src directory

--- src/utils/jwt.ts
+++
@@ -1,6 +0,0 @@
-import jwt from 'jsonwebtoken';
-import { env } from '../config/env';
-
-export function generateToken(payload: object): string {
-  return jwt.sign(payload, env.jwtSecret, { expiresIn: '1d' });
-}
